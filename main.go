206ca90ab518958a05b52b3508a1f9ababccd280
Deepak Jois <deepak.jois@gmail.com>
2011-06-29 17:12:42 -0700

    Working music playback in goroutine

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import "mp3"
 import "os"
+import "time"
+import "runtime"
 
 func main() {
-	mp3.DecodeTrack(os.Args[1])
+	go func() {
+		runtime.LockOSThread()
+		mp3.DecodeTrack(os.Args[1])
+	}()
+
+	<- time.Tick(5e9)
 }
